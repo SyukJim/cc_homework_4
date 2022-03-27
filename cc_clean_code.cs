ffe27433f6b65c20fad0f1da11ad5eb6fd2c59fb
SyukJim <98511989+SyukJim@users.noreply.github.com>
2022-03-27 19:37:53 +0800

    Add files via upload

---
+++ cc_clean_code.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace cc_clean_code_assignment4
+{
+    class Person
+    {
+
+        private string name;
+        private int age;
+
+        public void Insert(string n, int a)
+        {
+
+            name = n;
+            age = a;
+        
+        }
+
+        public void ShowData()
+        {
+
+            Console.WriteLine("Name: {0}, age: {1}.", name, age);
+        
+        }
+
+    } 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String s;
+            int a;
+
+            string personName;
+            int personAge;
+
+            Person p = new Person();
+
+            Console.Write("Enter your name: ");
+            s = Console.ReadLine();
+            personName = s;
+
+            Console.Write("Enter your age: ");
+            s = Console.ReadLine();
+            a = int.Parse(s);
+            personAge = a;
+
+            p.Insert(personName, personAge);
+            p.ShowData();
+
+
+        }
+    }
+}
